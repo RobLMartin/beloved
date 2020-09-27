229f9d4c6952f8d28900b08b85b20597d0a7ab13
Rob Martin <rmartin3@alumni.unca.edu>
2020-09-26 22:19:19 -0700

    Updates

---
+++ precache-manifest.8f2b8a591ffb5e4d06964c6427af2980.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cc5172b06e152c8f12c30f61d6df933c",
+    "url": "/beloved/index.html"
+  },
+  {
+    "revision": "95a447ede338cd1b8dde",
+    "url": "/beloved/static/css/main.523f09c5.chunk.css"
+  },
+  {
+    "revision": "2ba4defc82fbac572aa3",
+    "url": "/beloved/static/js/2.707d69e9.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/beloved/static/js/2.707d69e9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "95a447ede338cd1b8dde",
+    "url": "/beloved/static/js/main.e40d607a.chunk.js"
+  },
+  {
+    "revision": "a90b63c4e2b4dfc51e05",
+    "url": "/beloved/static/js/runtime-main.853dda7a.js"
+  }
+]);
